8341c1a3e8fdebf904bd8f7960ab8fe14f3af05f
John J. Camilleri <john@johnjcamilleri.com>
2012-12-04 09:50:48 +0100

    Work on parse module

--- Parse.hs
+++ Parse.hs
@@ -6,27 +6,53 @@ import Shell
 
 -- http://legacy.cs.uu.nl/daan/download/parsec/parsec.html
 
--- spaces :: Parser ()
--- spaces = skipMany1 space
+-- Better:
+-- http://book.realworldhaskell.org/read/using-parsec.html
 
-word :: Parser String
-word = many1 letter
+-- A shell value contains 0 or more commands, each of which is terminated
+-- by the pipe | character 
+-- shellVal :: GenParser Char st [[String]]
+-- shellVal = endBy command pipe
 
--- sepBy1 word (space <|> char '|')
+-- Each command contains 1 or more words, separated by spaces
+-- command :: GenParser Char st [String]
+shellVal = sepBy command pipe
+       
+-- Build up a list of words
+-- command :: GenParser Char st [String]
+command = sepBy word spaces --- TODO how to handle trailing whitespace?!
 
-pipeParse = (do
-  skipMany space
-  w <- word
-  skipMany space
-  return w) `sepBy` (char '|')
+word :: GenParser Char st String
+word = many1 alphaNum -- (noneOf "| ")
+       
+-- The pipe character is |
+--pipe :: GenParser Char st Char
+pipe =
+  do
+    char '|'
+    spaces
 
-shellParse = undefined
-  
+-- Parse a shell value
+--parseShellVal :: String -> Either ParseError [[String]]
+parseShellVal input = parse shellVal "(unknown)" input
 
--- readExpr :: String -> IO ()
-readExpr s = case parse shellParse "<interactive>" s of
-  Left err ->  undefined
-  Right val -> undefined
+------------------------------------------------------------------------------
+
+-- word :: Parser String
+-- word = many1 letter
+
+-- -- sepBy1 word (space <|> char '|')
+
+-- pipeParse = (do
+--   skipMany space
+--   w <- words
+--   skipMany space
+--   return w) `sepBy` (char '|')
+
+-- -- readExpr :: String -> IO ()
+-- readExpr s = case parse shellParse "<interactive>" s of
+--   Left err ->  undefined
+--   Right val -> undefined
 
 -- sentence    :: Parser [String]
 -- sentence    = do{ words <- sepBy1 word separator
