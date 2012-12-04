f38f341d222c31d4a360ddd3d8548ddb1eda2bca
Baldur Blöndal <baldur@ed-3358-05.studat.chalmers.se>
2012-12-04 16:09:21 +0100

    .

--- Shell.hs
+++ Shell.hs
@@ -1,87 +1,61 @@
-{-# OPTIONS_GHC -fglasgow-exts #-}
-
 module Shell where
 
 import System.Process
 import Control.Concurrent
 import Control.Concurrent.MVar
-import Control.Exception(evaluate)
-import System.Posix.Directory
-import System.Directory(setCurrentDirectory)
 import System.IO
 import System.Exit
 import Text.Regex
 import System.Posix.Process
 import System.Posix.IO
 import System.Posix.Types
-import Data.List
-import System.Posix.Env(getEnv)
+import Control.Monad
 
 type Arg = String
 
-data Command = Command String [Arg]
+newtype Annotation = Annotation String deriving Show
+
+data Command = Command String [Arg] Annotation 
   deriving Show
 
 data ShellVal = Pipe [Command]
   deriving Show
 
+data Result = Result { output :: IO String, status :: IO [ProcessStatus] }
 
-
-
-
-{- | The type for running external commands.  The first part
-of the tuple is the program name.  The list represents the
-command-line parameters to pass to the command. -}
-type SysCommand = (String, [String])
-
-{- | The result of running any command -}
-data CommandResult = CommandResult {
-    cmdOutput :: IO String,              -- ^ IO action that yields the output
-    getExitStatus :: IO ProcessStatus    -- ^ IO action that yields exit result
-    }
-
-{- | The type for handling global lists of FDs to always close in the clients
--}
 type CloseFDs = MVar [Fd]
 
-{- | Class representing anything that is a runnable command -}
-class CommandLike a where
-    {- | Given the command and a String representing input,
-         invokes the command.  Returns a String
-         representing the output of the command. -}
-    invoke :: a -> CloseFDs -> String -> IO CommandResult
+closeFds = mapM_ (\fd -> catch (closeFd fd) (const (return ())))
 
-run (cmd, args) input = do
-  (closefds :: MVar [Fd]) <- newMVar []
+invoke (cmd, args) closefds input = do
   (r1, w1) <- createPipe
   (r2, w2) <- createPipe
+  
   modifyMVar_ closefds (\old -> return (old ++ [w1, r2]))
   
   childPID <- withMVar closefds (\fds -> forkProcess (child (cmd, args) fds r1 w2))
   
-  closeFd r1
-  closeFd w2
+  closeFds [r1, w2]
 
-  stdinhdl <- fdToHandle w1
+  (stdinhdl, stdouthdl) <- liftM2 (,) (fdToHandle w1) (fdToHandle r2)
+  
   forkIO (hPutStr stdinhdl input >> hClose stdinhdl)
-  stdouthdl <- fdToHandle r2
 
-  hGetContents stdouthdl
+  let output = hGetContents stdouthdl
+      exit   = do
+        status <- getProcessStatus True False childPID
+        case status of
+          Nothing -> fail "Error: Nothing from getProcessStatus"
+          Just ps -> removeCloseFDs closefds [w1, r2] >> return [ps]
+
+  return (Result { output = output, status = exit })
 
 child (cmd, args) closefds r1 w2 = do
   dupTo r1 stdInput
   dupTo w2 stdOutput
-  closeFd r1
-  closeFd w2
-  mapM_ (\fd -> closeFd fd) closefds
+  closeFds (r1:w2:closefds)
   executeFile cmd True args Nothing
 
--- Add FDs to the list of FDs that must be closed post-fork in a child
-addCloseFDs :: CloseFDs -> [Fd] -> IO ()
-addCloseFDs closefds newfds =
-    modifyMVar_ closefds (\oldfds -> return $ oldfds ++ newfds)
-
--- Remove FDs from the list
 removeCloseFDs :: CloseFDs -> [Fd] -> IO ()
 removeCloseFDs closefds removethem =
     modifyMVar_ closefds (\fdlist -> return $ procfdlist fdlist removethem)
@@ -96,54 +70,21 @@ removeCloseFDs closefds removethem =
         | fd == x = xs
         | otherwise = x : removefd xs fd
 
-{- | Type representing a pipe.  A 'PipeCommand' consists of a source
-and destination part, both of which must be instances of
-'CommandLike'. -}
-data (CommandLike src, CommandLike dest) => 
-     PipeCommand src dest = PipeCommand src dest 
+--  (closefds :: MVar [Fd]) <- newMVar []
+pipe src dest closefds input = do
+  Result { output = output1, status = status1 } <- invoke' [src]  closefds input
+  Result { output = output2, status = status2 } <- invoke' dest closefds =<< output1
 
-{- | A convenient function for creating a 'PipeCommand'. -}
-(-|-) :: (CommandLike a, CommandLike b) => a -> b -> PipeCommand a b
-(-|-) = PipeCommand
+  return (Result output2 (liftM2 (++) status1 status2))
 
-{- | Make 'PipeCommand' runnable as a command -}
-instance (CommandLike a, CommandLike b) =>
-         CommandLike (PipeCommand a b) where
-    invoke (PipeCommand src dest) closefds input =
-        do res1 <- invoke src closefds input
-           output1 <- cmdOutput res1
-           res2 <- invoke dest closefds output1
-           return $ CommandResult (cmdOutput res2) (getEC res1 res2)
+invoke' [cmd] fds input = invoke cmd fds input
+invoke' (src:dst) fds input = pipe src dst fds input
 
-{- | Given two 'CommandResult' items, evaluate the exit codes for
-both and then return a "combined" exit code.  This will be ExitSuccess
-if both exited successfully.  Otherwise, it will reflect the first
-error encountered. -}
-getEC :: CommandResult -> CommandResult -> IO ProcessStatus 
-getEC src dest =
-    do sec <- getExitStatus src
-       dec <- getExitStatus dest
-       case sec of
-            Exited ExitSuccess -> return dec
-            x -> return x
-
-{- | Execute a 'CommandLike'. -}
-runIO :: CommandLike a => a -> IO ()
-runIO cmd =
-    do -- Initialize our closefds list
-       closefds <- newMVar []
-
-       -- Invoke the command
-       res <- invoke cmd closefds []
-
-       -- Process its output
-       output <- cmdOutput res
-       putStr output
-
-       -- Wait for termination and get exit status
-       ec <- getExitStatus res
-       case ec of
-            Exited ExitSuccess -> return ()
-            x -> fail $ "Exited: " ++ show x
+run cmd = do
+  closefds <- newMVar []
+  Result { output = output, status = status } <- invoke' cmd closefds []  
+  status >>= putStrLn . show
+  output >>= putStr
 
 
+commands = [("/bin/ls",[]), ("/bin/cat",[])]
