35de7c37521ae5f6d4fa712f489083728a0b0de2
John J. Camilleri <john@johnjcamilleri.com>
2012-12-05 20:59:24 +0100

    Parse quoted strings

--- Parse.hs
+++ Parse.hs
@@ -6,6 +6,11 @@ import Text.ParserCombinators.Parsec.Token (stringLiteral)
 import Prelude hiding (words)
 import Shell hiding (pipe)
 
+-- TODO ----------------------------------------------------------------------
+
+-- Parse stringliterals
+-- Change ShellVal to contain [Value] not [String]
+
 -- Examples ------------------------------------------------------------------
 
 type CmdParseObj = ([String], [String])
@@ -14,8 +19,20 @@ type CmdParseObj = ([String], [String])
 convert :: [CmdParseObj] -> PipeLine
 convert = Pipe . map convertCommand
 
-convertCommand ((c:cs),[]) = Command c cs []
-convertCommand ((c:cs),ts) = Command c cs [unwords ts]
+convertCommand (c:cs, ts) = Command c args types
+  where
+    -- args = map argToValue cs
+    args = cs
+    types = case ts of {
+      [] -> [] ;
+      _ -> [unwords ts]
+      }
+
+-- argToValue :: String -> Value
+-- argToValue s = case reads s :: ([Integer], String) of {
+--   (i, "") -> Int i ;
+--         _ -> Str s
+--   }
 
 -- A shell value contains 0 or more commands separated by the pipe | character
 pipeLine :: GenParser Char st [CmdParseObj]
@@ -37,10 +54,20 @@ annotation = do
   where capitalized = upper >>= \a -> many1 letter >>= return . (a:)
 
 -- A list of 1 or more words separated by spaces
-words = word `endBy1` spaces
-
+words = (quoted_string <|> word) `endBy1` spaces
 word = many1 (noneOf "| :")
-       
+  <?> "Single word"
+
+-- examples: "hello", "hello world"
+quoted_string = do
+  dquote
+  w <- many1 (noneOf "\\\"\r\n")
+  dquote
+  return w
+  <?> "Quoted string"
+
+dquote = char '"' <?> "Double quote"
+
 -- Parse a string into shell value, possibly returning a parse error
 parseInput :: String -> Either ParseError PipeLine
 parseInput input =
@@ -51,7 +78,7 @@ parseInput input =
 ------------------------------------------------------------------------------
 -- Testing
 -- Some valid commands for testing
-t1 = "c1 arg arg | c2 | c3 arg"
+t1 = "c1 arg \"hello world\" | c2 | c3 arg"
 t2 = "c1 arg arg :: t1 one | c2 :: t2 | c3 arg"
 
 -- Some invalid commands for testing
