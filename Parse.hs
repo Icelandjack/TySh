a9dbece35921fbf68117c4543393d2a7c8a37afc
John J. Camilleri <john@johnjcamilleri.com>
2012-12-05 21:30:14 +0100

    More cleanup to Parse.hs

--- Parse.hs
+++ Parse.hs
@@ -8,24 +8,22 @@ import Shell hiding (pipe)
 
 -- TODO ----------------------------------------------------------------------
 
--- Parse stringliterals
 -- Change ShellVal to contain [Value] not [String]
 
 -- Examples ------------------------------------------------------------------
 
-type CmdParseObj = ([String], [String])
+type ParsedCommand = ([String], [String]) -- (fun:args, annotation)
 
--- Creates a shell value from parser output
-convert :: [CmdParseObj] -> PipeLine
+-- Create ShellValue from parser output
+convert :: [ParsedCommand] -> PipeLine
 convert = Pipe . map convertCommand
-
-convertCommand (c:cs, ts) = Command c args types
+convertCommand (c:cs, as) = Command c args annot
   where
     -- args = map argToValue cs
     args = cs
-    types = case ts of {
+    annot = case as of {
       [] -> [] ;
-      _ -> [unwords ts]
+      _ -> [unwords as]
       }
 
 -- argToValue :: String -> Value
@@ -35,8 +33,10 @@ convertCommand (c:cs, ts) = Command c args types
 --   }
 
 -- A shell value contains 0 or more commands separated by the pipe | character
-pipeLine :: GenParser Char st [CmdParseObj]
-pipeLine = command `sepBy1` (pipe >> spaces)
+pipeLine :: GenParser Char st [ParsedCommand]
+pipeLine =
+  command `sepBy1` (pipe >> spaces)
+  <?> "pipeline"
   where pipe = char '|'
 
 -- A command contains 1 or more words separated by spaces
@@ -45,28 +45,38 @@ command = do
   cmd <- words
   ann <- option [] annotation
   return (cmd, ann)
+  <?> "command"
 
--- Type Annotations must be capitalized
+-- Type Annotations begin with :: and must be capitalized
 annotation = do
   string "::"
   spaces
-  capitalized `endBy1` spaces <?> "Capitalized word"
-  where capitalized = upper >>= \a -> many1 letter >>= return . (a:)
+  capitalized `endBy1` spaces 
+  <?> "type annotation"
 
--- A list of 1 or more words separated by spaces
+-- A capitalized word
+capitalized = do
+  a <- upper
+  as <- many1 alphaNum
+  return (a:as)
+  <?> "capitalized word"
+
+-- A list of 1 or more words/quoted strings
 words = (quoted_string <|> word) `endBy1` spaces
-word = many1 (noneOf "| :")
-  <?> "Single word"
+  <?> "word list"
 
--- examples: "hello", "hello world"
+-- a continuous sequence of characters (no spaces)
+word = many1 (noneOf "| :")
+  <?> "single word"
+
+-- a quoted string, eg "hello world"
 quoted_string = do
   dquote
   w <- many1 (noneOf "\\\"\r\n")
   dquote
   return w
-  <?> "Quoted string"
-
-dquote = char '"' <?> "Double quote"
+  <?> "quoted string"
+  where dquote = char '"'
 
 -- Parse a string into shell value, possibly returning a parse error
 parseInput :: String -> Either ParseError PipeLine
@@ -77,9 +87,10 @@ parseInput input =
 
 ------------------------------------------------------------------------------
 -- Testing
+
 -- Some valid commands for testing
 t1 = "c1 arg \"hello world\" | c2 | c3 arg"
-t2 = "c1 arg arg :: t1 one | c2 :: t2 | c3 arg"
+t2 = "c1 arg arg :: Type One | c2 :: Type2 | c3 arg"
 
 -- Some invalid commands for testing
 x1 = "c1 arg arg | c2 | "
